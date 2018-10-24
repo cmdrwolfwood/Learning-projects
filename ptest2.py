b6f046b2e29831a31b26e353def04bf82dcadc56
cmdrwolfwood <43388827+cmdrwolfwood@users.noreply.github.com>
2018-10-24 18:07:58 -0500

    Add files via upload
    added to do's, corrected styling to PEP8 compliant 80 character lines, found new errors to work on

--- ptest2.py
+++ ptest2.py
@@ -1,20 +1,31 @@
 # TODO make the alpha 0.05 a changeable variable by user
-# TODO give a 'close to' range for alpha variable that implies ambiguity in final determination
+# TODO give a 'close to' range for alpha variable that implies ambiguity in
+# final determination
 # TODO be more verbose in explanation of can/can't reject null hypotheses
+# TODO add elif statement for input that isn't capable of being made a float
+# TODO add statements for if the input value equals the alpha value exactly
+
+
 def pvalue():
+
     alpha = input('Input your alpha value:')
-    # this provides an input screen to enter p-values determined statistically from research data
+    # this provides an input screen to enter p-values determined statistically
+    # from research data
     alpha = float(alpha)
-    # this turns the input into a float value for comparison on the next few lines
+    # this turns the input into a float value for comparison
     if alpha > 0.05:
-        print('P-value is greater than 0.05 indicating the null hypothesis can be rejected')
-    # these lines compare the entered input against the 95% confidence value thats industry standard
+        print('P-value is greater than 0.05 indicating the null hypothesis can '
+              'be rejected')
+    # these lines compare the entered input against the 95% confidence value
+    # thats industry standard
     elif alpha < 0.05:
-        print('P-value is less than 0.05 indicating high confidence and that the null hypothesis cant be rejected')
+        print('P-value is less than 0.05 indicating high confidence and that '
+              'the null hypothesis cant be rejected')
 
 
-print('Always publish your P-value with any paper, any ambiguity can be critiqued and considered.')
-# starting the program with pvalue, allowing input to be entered, then states that 2nd print line for consideration
-# each time its published/used as a program
+print('Always publish your P-value with any paper, any ambiguity can be '
+      'critiqued and considered.')
+# states that print line for consideration each time its used
 
 pvalue()
+# runs the function
